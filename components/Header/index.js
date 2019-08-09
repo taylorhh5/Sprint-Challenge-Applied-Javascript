46c9880d27a393ce5b6af5344cbc31de29b1f7ef
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-08-09 11:50:26 -0400

    added to headerjs

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,37 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const headCon = document.querySelector(".header-container")
+const headerFunction = Header ();
+headCon.appendChild (headerFunction);
+
+function Header() {
+ 
+//create elements
+const divHeader = document.createElement('div');
+const dateSpan = document.createElement('span');
+const header2 = document.createElement('h1');
+const spanTemp = document.createElement('span');
+
+
+
+//structure
+divHeader.appendChild(dateSpan)
+divHeader.appendChild(header2)
+divHeader.appendChild(spanTemp)
+
+
+//class names
+divHeader.classList.add('header')
+dateSpan.classList.add('date')
+spanTemp.classList.add('temp')
+
+
+
+//text content
+dateSpan.textContent = "MARCH 28, 2019"
+header2.textContent ="Lambda Times"
+spanTemp.textContent ="98°"
+
+return divHeader
+}
