0f157d1b3518d0e02f537547e5a65b5d8e470730
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-08-09 13:58:15 -0400

    added to cardsJS

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,86 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+  
+
+//need to loop through array of articles
+//loop again and get specific info
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then((response) => {
+    console.log(response);
+    response.data.articles.bootstrap.forEach (info=> {
+        const newArticle = articleCreate (info);
+            const articleCon=document.querySelector(".cards-container")
+            articleCon.appendChild (newArticle);
+
+        })
+    })
+
+      
+
+// axios.get('https://lambda-times-backend.herokuapp.com/articles')
+// .then((response) => {
+//     console.log(response);
+//     response.data.articles.javascript.forEach (info=> {
+//         const newArticle = articleCreate (info);
+//             const articleCon=document.querySelector(".cards-container")
+//             articleCon.appendChild (newArticle);
+//         })
+//     })
+
+  
+
+//     axios.get('https://lambda-times-backend.herokuapp.com/articles')
+//     .then((response) => {
+//         console.log(response);
+//         response.data.articles.jquery.forEach (info=> {
+//             const newArticle = articleCreate (info);
+//                 const articleCon=document.querySelector(".cards-container")
+//                 articleCon.appendChild (newArticle);
+//             })
+//         })
+    
+
+
+
+function articleCreate (info) {
+
+//define new elements
+const divCard = document.createElement('div');
+const divHeadline = document.createElement('div');
+const divAuthor =document.createElement('div');
+const imgC = document.createElement('div');
+const imgSrc =document.createElement('img');
+const spanName = document.createElement('span');
+
+//structure
+divCard.appendChild(divHeadline)
+divCard.appendChild(divAuthor)
+divAuthor.appendChild(imgC)
+imgC.appendChild(imgSrc)
+divAuthor.appendChild(spanName)
+
+//class names
+divCard.classList.add('card')
+divHeadline.classList.add('headline')
+divAuthor.classList.add('author')
+imgC.classList.add('img-container')
+
+
+
+//set text content 
+divHeadline.textContent = info.headline;
+divAuthor.textContent = info.authorName;
+imgSrc.src = info.authorPhoto;
+
+
+
+
+
+return divCard
+
+
+}
+
